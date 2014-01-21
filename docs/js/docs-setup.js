49556a5249c6b28a157930ddc1aed9e6b2492fc9
service@deaneturner.com <service@deaneturner.com>
2014-01-20 22:34:50 -0500

    docs

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,129 @@
+NG_DOCS={
+  "sections": {
+    "tutorial": "Guides",
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "tutorial",
+      "id": "Developer Notes",
+      "shortName": "Developer Notes",
+      "type": "overview",
+      "keywords": "$rootscope access accuracy addresses after allows also and angularjs api are as assure based be both bower breaking browser build builds built by callbacks can carrier list change changes code combination company companyname compass compilation compile compliance consistency console css custom description details dev developer development directory distribution docs documentation dynamic each easy errors etc exception exceptionhandler execution failure failures fatal feature features file files following for full function generate generated git global good grunt guide handled handling href html i18nnotifications id important in index info inspecting installation instructions integrations interceptor into is it javascript js jshint key keys language level levels list load locales located logging main match material md message minification minified modifying monitor necessary need node non-fatal not note notes notifications notifies notify occur of on opening or overview periodically prerequisite proceed production profile project properly pushforcurrentroute readme reasons recompile refreshing related relevant required requires resultant root run running sass schedule seamlessly see serve server service severity source start stored structure styles success successful successfully supports susceptible task tasks technologies test tests that the these this those to toastr tomcat triggered tutorial ui under unit user using various verification via warning watch well when will without workflow working write wrong xhr yeoman"
+    },
+    {
+      "section": "tutorial",
+      "id": "Documentation",
+      "shortName": "Documentation",
+      "type": "overview",
+      "keywords": "_blank and annotation annotations api are as be building code com content contents dev directory docs documentation documents facets file files for from generate generated generating grunt grunt-ngdocs guides href html http https index into is it js jsdoc_examples markdown means ngdoc ngdocs of open or org overview project provides qooxdoo root serve shorthand some stored target tasks the these this to tool tutorial under used using via view web well will"
+    },
+    {
+      "section": "tutorial",
+      "id": "Exception Handling",
+      "shortName": "Exception Handling",
+      "type": "overview",
+      "keywords": "$scope $watch according alert alert- all alleviate alleviates allow allows also alter an and anglularjs angular angularjs application are array authentication automatically both by callback callbacks calls can list carrierselector case cases ccapp class close code common company console contain curnotifications data default dependency details display displaying enabling encompassing error errors etc exception exceptionhandler exceptions explicit external failure fatal file_list for foreach format function getlist getcurrent global handle handler handling has href hrp html http i18nnotifications id if in include incomplete index inspecting integrates integration interceptor is it javascript key length localization log many means message messages module modules moment most msgkey native necessary need ng-class ng-click ng-repeat no non-fatal note notification notifications notify object objects occurred of one options or overview parameter params predefined presents primary proper provide provides purpose reading reads regarding removenotification replacement response schedule see server service status string success templates that the this throw throwing time to toastr tools top-level translation tutorial type ui unrecoverable usage use user users uses using var via warning watching will with write wrong xhr"
+    },
+    {
+      "section": "tutorial",
+      "id": "Locales",
+      "shortName": "Locales",
+      "type": "overview",
+      "keywords": "an and are authentication authorization based carrier list carriers change changeerror chart column company connect current dashboard someco descrepancies description discrepancies due error errors events example fatal feature features file footer footers for function functional general has header id ii incomplete internal is keys latest left list locales metrics name nav non-fatal notice occurred on overview panel progress rail reports resource retrieve route schedule server status structural structure subject tab table title to transmission tutorial unable unrecoverable version wrong"
+    },
+    {
+      "section": "tutorial",
+      "id": "Localization",
+      "shortName": "Localization",
+      "type": "overview",
+      "keywords": "affect all an and angular application bar be cases ccapp common dependency during en enabling encompassing equal exception exceptionhandler expressions filters first firstname foo for from get handling href html i18n i18nmessages in include index init initialize initialized intialization is javascript key last lastname locales localization localizationmessages localizedmessages main markup message module modules most must my name notification notifications of overview pairs portion proper provide render retrieve run see startup string the to top-level translation translations tutorial typically used value will"
+    },
+    {
+      "section": "tutorial",
+      "id": "Notificatons",
+      "shortName": "Notifications",
+      "type": "overview",
+      "keywords": "$on $routechangeerror $scope $watch above alert alert- all an and anglularjs angular angularjs api appctrl application are array as be but by bypassing can carrier list cases ccapp change changeerror class close closebutton common company completed config configure controller curnotifications current custom define delegate deligate dependency described display displaying do efficient enabling encompassing error errors etc exception exceptionhandler exceptionhandlerfactory exceptions exceptonhandler explanation extend external fadeout fatal for foreach format function generating getcurrent handled has href html i18nnotifications i18nnotificationsettings id if in include index instead integrating integration integrations is javascript key length levels list localization main many match means menuctrl message method module modules moment most msgkey native navbar navbar-default navbar-fixed-top necessary ng-class ng-click ng-controller ng-repeat non-fatal nonfatal not notification notifications notify object occurred of on optional options or overview params phase presents previous proper provide providean provides pushforcurrentroute rejection related removenotification route routechange run schedule seamlessly see service set settings severity single stacked stored success templates the this those throw time to toastr together tools top-level trigger triggered true tutorial type ui under unrecoverable use var various version vs warning watch watching will with wrong xhr"
+    },
+    {
+      "section": "tutorial",
+      "id": "UserProfile",
+      "shortName": "UserProfile",
+      "type": "overview",
+      "keywords": "$rootscope access and angular application ccapp common companyname dependency easy enabling for include info information is later loads manages module on or overview profile proper service stored the top-level tutorial user userprofile"
+    },
+    {
+      "section": "api",
+      "id": "common.exceptionHandler:exceptionHandler",
+      "shortName": "exceptionHandler",
+      "type": "object",
+      "keywords": "$scope according also an and api are authentication both by callback calls can list carrierselector case code common company console contain containing data default details error errors exceptionhandler failure fatal file_list for getlist global handle handler hrp http i18nmessages i18nnotifications id incomplete inspecting interceptor is it log many messages msgkey necessary no non-fatal notification notify object objects of one or parameter params predefined provide provides regarding replacement response schedule see server service status string success the to translation ui user uses via will wrong xhr"
+    },
+    {
+      "section": "api",
+      "id": "common.exceptionHandler:exceptionHandlerFactory",
+      "shortName": "exceptionHandlerFactory",
+      "type": "object",
+      "keywords": "alter an and api both common console default error errors etc exception exceptionhandler exceptions fatal for format handler has i18nnotifications in instead javascript key log means message moment msgkey non-fatal note notifications notify object occurred of overrides params produce provides service success that the this throw throwing throws time to type ui unrecoverable use user utilize via warning will"
+    },
+    {
+      "section": "api",
+      "id": "common.filters:i18n",
+      "shortName": "i18n",
+      "type": "filter",
+      "keywords": "an and api bar be common equal expression expressions filter filters first firstname foo for i18n in injected into is key last lastname localization message msgkey my name parameters replacement return string the to translated using value will"
+    },
+    {
+      "section": "api",
+      "id": "common.i18nMessages:i18nMessages",
+      "shortName": "i18nMessages",
+      "type": "service",
+      "keywords": "an and api application as base be callback can changed common demand during early en execute for from get gets given i18nmessages init initialize initialized key lang language locales localization localized locally messages method module necessary object on pair pairs path phase provides reloads retrieve run service setlang sets should startup stores strings success the to translated translation uri use used value via which xhr"
+    },
+    {
+      "section": "api",
+      "id": "common.i18nNotifications:i18nNotifications",
+      "shortName": "i18nNotifications",
+      "type": "service",
+      "keywords": "$scope adding adds all and any api array arrays be before by carrier list closebutton common company config containing current delegate deligate desired error errors example fadeout for form from function getcurrent gets i18n i18nnotifications id in injected into is javascript key list localizedmessages message method msgkey next no notification notifications notify obj object of on options or parameters passing performs preparenotifications prepares pushforcurrentroute pushfornextroute pushsticky remove removes replacement required responsibility route schedule service setdelegate sets sticky storage string the this to toastr translated translating translations true type use using value warning when which without wrapper wrong"
+    },
+    {
+      "section": "api",
+      "id": "common.localizedMessages:localizedMessages",
+      "shortName": "localizedMessages",
+      "type": "service",
+      "keywords": "additional an and api bar be common equal first firstname foo for get gets given i18messages in injected into is javascript key last lastname localizationmessages localized localizedmessages message messages method msgkey my name parameter parameters provides replacement replament retrieve return service set string strings support the to translated used using value will with"
+    },
+    {
+      "section": "api",
+      "id": "common.notifications:notifications",
+      "shortName": "notifications",
+      "type": "service",
+      "keywords": "$scope above according across add added addnotification adds all an and any api applicable are array arrays available be become carried change changes closebutton common config consisting current deligate desired directly error example explicitly fadeout for form from function getcurrent gets given has hold holds i18nnotifications if in is lost manage message method must next none notification notificationobj notifications notificationsarray notifies notify object of on option options or otherwise over persist persistence persists property pushforcurrentroute pushfornextroute pushsticky remove removeall removed removes required route routes service sticky storage stored successful the they this tne to toastr transferred true type until upon use using via where which without"
+    },
+    {
+      "section": "api",
+      "id": "common.services:session",
+      "shortName": "session",
+      "type": "service",
+      "keywords": "addconfig api common config current http log logout manage method notifications object of out service services session the to user"
+    },
+    {
+      "section": "api",
+      "id": "common.userProfile:userProfile",
+      "shortName": "userProfile",
+      "type": "service",
+      "keywords": "$rootscope access addconfig and api common companyname config easy employee for getinfo http info information init initializes is it load manage method object on or profile retrieves root scope service stored stores the to user userprofile"
+    }
+  ],
+  "apis": {
+    "tutorial": false,
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
